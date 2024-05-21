dd38445affa31974dba5824d453bbaafaa9ef611
Manuel Silva <80219378+manuseiro@users.noreply.github.com>
2024-05-20 22:05:37 -0300

    incluindo o PUC

--- functions.php
+++ functions.php
@@ -1,3 +1,15 @@
 <?php
-// Incluir o arquivo de verificação de atualizações do tema
-require get_template_directory() . '/update/theme-update.php';
+require 'plugin-update-checker/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/manuseiro/lipaGPT/',
+	__FILE__,
+	'lipagpt'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('your-token-here');
